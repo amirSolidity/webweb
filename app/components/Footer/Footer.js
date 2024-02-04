b57d0c442b21fa014c50ac5bf864dc0ef40c1426
amirreza niyazi <amirnyz11@gmail.com>
2024-02-04 16:02:05 +0330

    start

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,10 +1,48 @@
+import { FaTelegram } from "react-icons/fa";
+import { LuInstagram } from "react-icons/lu";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { TiSocialTwitter } from "react-icons/ti";
 import './Footer.css'
 
 
-const Footer = ()=>{
-    return(
+const Footer = () => {
+    return (
         <div className='footerContainer'>
+            <div className='footerMenuDiv'>
+                <div className='footerLogoDiv'>
+                    <img className='footerLogo' src='./logoImg2.png' />
+                </div>
+                <div className='footerMenu'>
+                    <span className='menuOption'>Home</span>
+                    <span className='menuOption'>Crypto Price</span>
+                    <span className='menuOption'>Tweets</span>
+                    <span className='menuOption'>NFT</span>
+                </div>
+                <div className='footerMenu'>
+                    <span className='menuOption'>Notes</span>
+                    <span className='menuOption'>Favorite Cryptos</span>
+                    <span className='menuOption'>Wallet</span>
+                    <span className='menuOption'>Profile</span>
+                </div>
 
+                <div className='footerEmailDiv1'>
+                    <span className='footerEmailTxt'>Enter Your Email Address</span>
+                    <div className='footerEmailDiv'>
+                        <input type='email' Name='footerEmailInput' placeholder='Email Address' />
+                        <button className='footerBtn'>Send</button>
+                    </div>
+                </div>
+            </div>
+            <div className='footerLine'></div>
+            <div className='footerSocialMediaDiv'>
+                <span className='footerTxt'>ThemeREX © 2024. All rights reserved.</span>
+                <div className="footerIconsDiv">
+                    <FaTelegram className="footerIcon" />
+                    <LuInstagram className="footerIcon" />
+                    <FaLinkedinIn className="footerIcon" />
+                    <TiSocialTwitter className="footerIcon" />
+                </div>
+            </div>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,23 +1,53 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import './Home.css'
-import { motion } from "framer-motion";
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer';
 
 
 function Home() {
 
 
-  const icon = {
-    hidden: {
-      pathLength: 0,
-      fill: "rgba(255, 255, 255, 0)"
+  const servicesList = {
+    "cryptoPrice": {
+      "name": "crypto Price",
+      "img": "./cryptoPriceImg.jpg",
+      "description": "On this site, you can find out about the price and price changes of various cryptocurrencies.",
     },
-    visible: {
-      pathLength: 1,
-      fill: "rgba(255, 255, 255, 1)"
-    }
+    "tweets": {
+      "name": "Tweets",
+      "img": "./tweetImg.jpg",
+      "description": "You can see the tweets shared about the Ethereum network and learn about updates and changes",
+    },
+    "nft": {
+      "name": "NFT",
+      "img": "./nftS.jpg",
+      "description": "You can view and check all types of NFT collections along with their prices and details",
+    },
+    "saveCrypto": {
+      "name": "Favorite Crypto",
+      "img": "./saveCryptoImg.jpg",
+      "description": "You can view all your digital assets in your profile and manage them.",
+    },
+    "wallet": {
+      "name": "Crypto Wallet",
+      "img": "./walletImg.jpg",
+      "description": "You can view all your digital assets in your profile and manage them.",
+    },
+    "note": {
+      "name": "Notes",
+      "img": "./noteImg.jpg",
+      "description": "You can make your own notes about tweets. Add transfers and... to your profile",
+    },
   }
+
+  const setServices = Object.keys(servicesList).map((key) => ({
+    name: servicesList[key].name,
+    img: servicesList[key].img,
+    description: servicesList[key].description,
+  }))
+
+
 
   return (
     <div className='homeContainer'>
@@ -28,23 +58,45 @@ function Home() {
           <img className='homeImg ll' src='nftImg.jpg' />
         </div>
 
-        <div className='txtDiv1'>
-          <span className='desTxt1'>
-            This website was created to provide web3 and blockchain services.
-            You can also use this website for transfers and blockchain news and digital currencies.
-            Click the button for more details.
-          </span>
+        <div className='homeDesBtnDiv'> 
+          <div className='txtDiv1'>
+            <span className='desTxt1'>
+              This website was created to provide web3 and blockchain services.
+              You can also use this website for transfers and blockchain news and digital currencies.
+              Click the button for more details.
+            </span>
+          </div>
+
+          <div className='desBtnDiv'>
+            <Button variant="success" className='detailsBtn'>Details</Button>
+          </div>
         </div>
 
-        <div className='desBtnDiv'>
-          <Button variant="success" className='detailsBtn'>Details</Button>
+      </div>
+
+      <div className='servicesDiv'>
+        <h1 className='servicesHeadTxt'>Our Services</h1>
+        <div className='services'>
+          {setServices.map((item) => {
+            return (
+              <div className='sevice'>
+                <div className='serviceImgDiv'>
+                  <img className='serviceImg' src={item.img} />
+                </div>
+                <div className='serviceDesDiv'>
+                  <h1 className='serviceDesHeader'>{item.name}</h1>
+                  <span className='serviceDes'>
+                    {item.description}
+                  </span>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
-      <motion.div
-      className="box"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-    />
+
+      <Footer />
+
     </div>
   )
 }
